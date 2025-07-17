e53d2c39a9ef77b8dfe498b17e51b3e98b0742ab
Narayan Pradhan <narayanp@mindfiresolutions.com>
2025-07-17 10:41:05 +0530

    feat: Add 404 page for unregistered routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import DoctorDashboard from "./pages/dashboards/DoctorDashboard";
 import PatientDashboard from "./pages/dashboards/PatientDashboard";
 import "./App.css";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
+import { NotFound } from "./pages/Notfound";
 
 function App() {
   return (
@@ -25,7 +21,7 @@ function App() {
           <Route path="/doctor" element={<DoctorDashboard />} />
           <Route path="/patient" element={<PatientDashboard />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
